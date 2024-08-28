37f7b7859cb66954863cece338841ebd2cddcd1a
Joe <jibarra8917@gmail.com>
2024-08-28 16:46:42 -0600

    first commit

---
+++ src/functions/timerTrigger1.js
@@ -0,0 +1,9 @@
+const { app } = require('@azure/functions');
+
+app.timer('timerTrigger1', {
+    schedule: '0 */1 * * * *',
+    handler: (myTimer, context) => {
+        context.log('Timer function processed request.');
+        timerFunction(context, myTimer);
+    }
+});
